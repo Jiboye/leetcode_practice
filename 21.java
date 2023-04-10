73f6e9fb1c0cc61098f2f8a714c55bd71a02b718
Jiboye <zxc1996821@gmail.com>
2023-04-10 11:41:21 +0800

    fixed error

--- 21.java
+++ 21.java
@@ -8,15 +8,17 @@ public class ListNode{
 
 class Solution{
     public ListNode mergeTwoLists(ListNode list1 ,ListNode list2){
-        if(list1.val >= list2.val){
-            list1 = list1.next;
+        if(list1 == null)
+            return list2;
+        if(list2 == null)
+            return list1;
+        if(list1.val <= list2.val){
+            list1.next = mergeTwoLists(list1.next,list2);
+            return list1;
         }
         else{
-            ListNode tmp = new ListNode(list2.val);
-            tmp.next = list1.next.next;
-            list1.next = tmp;
-            list2 = list2.next();
+            list2.next = mergeTwoLists(list1,list2.next);
+            return list2;
         }
-        return list1;
     }
 }
