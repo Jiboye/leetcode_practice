fddf66e8424bd3ae1643117958cac99ef4f6315e
Jiboye <zxc1996821@gmail.com>
2023-04-11 16:14:32 +0800

    fixbug

--- 2391.java
+++ 2391.java
@@ -1,14 +1,17 @@
+import java.util.*;
+import java.io.*;
+
 class Solution{
     public String RemoveStarsStackMethod(String s){
-        Stack<char> st = new Stack<char>;
-        for(int i = 0 ; i < s.length;i++){
+        Stack<Character> st = new Stack<Character>();
+        for(int i = 0 ; i < s.length();i++){
             if(s.charAt(i) == '*')
                 st.pop();
             else
                 st.push(s.charAt(i));
         }
-        char[] result = new char[st.size];
-        for(int j = st.size -1;j >=0;j--){
+        char[] result = new char[st.size()];
+        for(int j = st.size() -1;j >=0;j--){
             result[j] = st.pop();
         }
         return new String(result);
@@ -16,14 +19,14 @@ class Solution{
     public String RemoveStarsStringMethod(String s){
         int j = 0;
         StringBuilder str = new StringBuilder();
-        for(int i = 0;i < s.length ; i++){
+        for(int i = 0;i < s.length() ; i++){
             if(s.charAt(i) == '*'){
-                str.deleteCharAt(str.length-1);
+                str.deleteCharAt(str.length()-1);
             }
             else{
                 str.append(s.charAt(i));
             }
         }
-        return str;
+        return str.toString();
     }
 }
