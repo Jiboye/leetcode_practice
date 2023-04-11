80ba0ac9cd168ab6b212edfa17f6a6a37774e359
Jiboye <zxc1996821@gmail.com>
2023-04-11 16:46:47 +0800

    add comment

--- 844.java
+++ 844.java
@@ -7,15 +7,15 @@ class Solution{
     public String helper(String s){
         StringBuilder ss = new StringBuilder();
         for(int i = 0 ; i < s.length();i++){
-            if(s.charAt(i) == '#'){
+            if(s.charAt(i) == '#'){ // Check if Character is BackSpace
                 if(ss.length()>0){
-                    ss.deleteCharAt(ss.length()-1);
+                    ss.deleteCharAt(ss.length()-1); //if string is not empty, delete the last Character in string
                 }
                 else
-                    continue;
+                    continue; // If String is empty, do nothing.
             }
             else{
-                ss.append(s.charAt(i));
+                ss.append(s.charAt(i)); //Add Character into string
             }
         }
         return ss.toString();
